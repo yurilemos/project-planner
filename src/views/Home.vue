b25e97b75ca46a5dbb07579f5c64a80357313c9b
yurilemos <56370419+yurilemos@users.noreply.github.com>
2021-02-01 23:54:18 -0300

    commit on 'master'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project" @delete="handleDelete" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+import SingleProject from '../components/SingleProject1.vue'
+  export default {
+    name: 'Home',
+    components: { SingleProject },
+    data() {
+      return {
+        projects: []
+      }
+    },
+    mounted() {
+        fetch('http://localhost:3000/projects')
+          .then(res => res.json())
+          .then(data => this.projects = data)
+          .catch(err => console.log(err.message))
+    },
+    methods: {
+      handleDelete(id) {
+        this.projects = this.projects.filter((project) => {
+          return project.id !== id
+        })
+      }
+    }
+    
   }
-}
 </script>
